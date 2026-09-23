80bb1b43d549d0de7da3ea4971c8b7a91fa5dd69
Ahmed Ali <ahmed.ali@autotrader.co.uk>
2026-09-23 16:42:20 +0100

    Session 1

--- src/main/java/org/mdigital/Main.java
+++ src/main/java/org/mdigital/Main.java
@@ -1,9 +1,47 @@
 package org.mdigital;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
 public class Main {
     static void main() {
-        System.out.println("Hello C");
+
+//        int age = 20; // whole number
+//        String name = "Ciel"; // text (must have double quote) single '' = char
+//        double price = 9.99; // decimal number
+//        final int nage = 18; // constant variable (never changes)
+//
+//        System.out.println(nage);
+//        System.out.println("Hello World!");
+//        System.out.println(age);
+//        System.out.println(name);
+//        System.out.println(age + name + price);
+
+        String name = "Ciel";
+
+        System.out.println(name.substring(1,3)); // (use name. for all methods)
+
+        String sentence = " the quick fox jumps ";
+        String[] ls = sentence.split(" ");
+
+        HashSet<String> movies = new HashSet<>(); // has no dups
+        movies.add("spiderman");
+        movies.add("batman");
+        movies.add("superman");
+        movies.add("hero");
+        movies.add("spiderman");
+
+        System.out.println(movies);
+        System.out.println(movies.contains("hero"));
+
+        LinkedHashMap<String, String> mtitle = new LinkedHashMap<>();
+        mtitle.put("spiderman", "2026, ciel");
+        mtitle.put("batman", "1999, adam");
+        mtitle.put("43", "5000, cole");
+        mtitle.put("superman", "2030, ci");
+        System.out.println(mtitle);
+        System.out.println(mtitle.get("batman"));
+        System.out.println(mtitle.containsKey("superman"));
+
     }
 }
