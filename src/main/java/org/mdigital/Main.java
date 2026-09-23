563b25f0324356d920b21b66b3da4cd5dc39bf7f
Ahmed Ali <ahmed.ali@autotrader.co.uk>
2026-09-23 12:15:04 +0100

    Final commit

--- src/main/java/org/mdigital/Main.java
+++ src/main/java/org/mdigital/Main.java
@@ -4,6 +4,6 @@ package org.mdigital;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        System.out.println("Hello Ahmed");
+        System.out.println("Hello C");
     }
 }
